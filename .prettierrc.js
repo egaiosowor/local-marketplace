a4b68fa166434c6f8f497b69739f8d36e85855c6
egaiosowor <egaiosowor@yahoo.com>
2025-06-06 16:57:58 +0100

    feat: clean up working directory and setup eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 80,
+};
+
+export default config;
