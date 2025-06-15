6d8a26a78417a0c22c04dea305be3bbb204e2f0f
egaiosowor <egaiosowor@yahoo.com>
2025-06-15 04:21:54 +0100

    feat: generate metadata for listing details page

--- src/app/listings/[id]/page.tsx
+++ src/app/listings/[id]/page.tsx
@@ -1,10 +1,27 @@
 import { ListingDetailsScreen } from "./screen";
+import { listingServices } from "@/features/listings/services/listing-services";
+import { ListingSchema } from "@/features/listings/listing.validation";
 import type { Metadata } from "next";
+import { env } from "@/config/env";
 
-// export const metadata: Metadata = {
-//     title: :Listing
-// }
+type Props = {
+  params: { id: string };
+};
 
-export default function ListingDetailsPage() {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const param = await params;
+  const id = await param.id;
+  try {
+    const data = await listingServices.getListingById(id);
+    const listing = ListingSchema.parse(data);
+    const title = listing.title || "Listing";
+    return { title: `${title} | ${env.APP_NAME}` };
+    //eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { title: `Listing | ${env.APP_NAME}` };
+  }
+}
+
+export default async function ListingDetailsPage() {
   return <ListingDetailsScreen />;
 }
